2d0513cf3cac5a2adf148013cb903c8c4c103e0b
meganaboddam <meganaboddam@gmail.com>
2023-09-04 16:44:57 -0700

    js app for solver

--- src/app.js
+++ src/app.js
@@ -1,7 +1,13 @@
 const express = require("express");
 const app = express();
 
+app.get('/', (req, res)=>{
+    res.status(200);
+    res.send("Welcome to Sudoku Solver");
+});
+
 app.get("/test", (_req, res) =>  {
   res.status(200).send("Hello world")
 })
+
 module.exports = app;
--- src/index.js
+++ src/index.js
@@ -1,6 +1,26 @@
-const app =  require( "./app");
-const port = process.env.PORT || 3000;
+const app = require( "./app");
+const PORT = process.env.PORT || 3000;
+const axios = require('axios').default
+const express = require('express')
+const cors = require('cors')
+app.use(cors())
+require('dotenv').config()
+app.use(express.json())
 
-app.listen(port, () =>
-  console.log('Example app listening on port 3000!'),
-);
+app.post('/solve', (req, res) => {
+  console.log(req.body)
+  const options = {
+    method: 'POST',
+    data: {
+      puzzle: req.body.numbers
+    }
+  };
+  axios.request(options).then((response) => {
+    console.log(response.data)
+    res.json(response.data)
+  }).catch((error) => {
+    console.error(error)
+  })
+})
+
+app.listen(PORT, () => console.log(`server running on PORT ${PORT}`))
